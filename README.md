8bdc65128c359f5e6d01b65622c239569a10f9ad
Lucas Miguel Ruiz <lucasmruiz05@gmail.com>
2022-01-15 19:39:15 -0300

    Welcome section finished

--- README.md
+++ README.md
@@ -3,5 +3,4 @@
 Para éste desafío debo realizar un sitio web responsivo e interactivo en el cual se encuentre mi información cómo desarrollador.
 El sitio contará con cuatro(4) páginas. Un index, portfolio, servicios y contacto.
 Debe consumir API's de un CMD(contentfull) realizado por mi.
-
 https://lksr-dev.github.io/my-site/
--- welcome.js
+++ welcome.js
@@ -1,14 +1,45 @@
-function getData() {
+function addDataW(result) {
+  const templateEl = document.querySelector("#welcome");
+  const container = document.querySelector(".hero");
+
+  templateEl.content.querySelector(".hero__title").textContent = result.title;
+
+  const clone = document.importNode(templateEl.content, true);
+  container.appendChild(clone);
+}
+
+function getDataW() {
   return fetch(
     "https://cdn.contentful.com/spaces/lwf2ooxtsp3z/environments/master/entries?access_token=FWhYWbWoO-JmpXm3YnrAtPwSXKBfaUnCasvJ-f7gb0Q&content_type=welcome"
   )
     .then((data) => {
       return data.json();
     })
-    .then((json) => console.log(json));
+    .then((json) => {
+      console.log(json);
+      const items = json.items.map((i) => {
+        const imgId = i.fields.img.sys.id;
+        const img = getImgW(imgId, json);
+        return {
+          title: i.fields.title,
+          image: img.fields.file.url,
+        };
+      });
+      return items;
+    });
+}
+
+function getImgW(id, json) {
+  return json.includes.Asset.find((i) => {
+    return i.sys.id == id;
+  });
 }
 
 function main() {
-  getData();
+  getDataW().then(function (works) {
+    for (const w of works) {
+      addDataW(w);
+    }
+  });
 }
 main();
